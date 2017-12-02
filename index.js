7b7a7bbe8a125a43cb1fb876642a9f706c1b9a2c
Nguyễn Hùng Nhuần <ivannguyen.it@gmail.com>
2017-12-02 11:27:11 +0700

    [add] structure

--- index.js
+++
@@ -1,7 +0,0 @@
-const coap = require("coap");
-const app = require("./app");
-const server = coap.createServer(app);
-const port = '9000'
-server.listen(port,() => {
-    console.log("The CoAP server is now running.\n" + app.help);
-});
