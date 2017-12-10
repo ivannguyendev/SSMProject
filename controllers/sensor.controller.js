8f66e76b317a89726fb5e46e316801e66d1f35c8
Nguyễn Hùng Nhuần <ivannguyen.it@gmail.com>
2017-12-10 12:43:15 +0700

    [update] check ip

--- controllers/sensor.controller.js
+++ controllers/sensor.controller.js
@@ -1,5 +1,6 @@
 var forEach = require('lodash.foreach');
 var ObjectID = require('mongodb').ObjectID;
+var checkip = require('./checkip')
 
 const   mongoose = require('mongoose'),
         sensorlModel = require('../model/Sensor'),
@@ -8,27 +9,30 @@ const   mongoose = require('mongoose'),
 
 module.exports = { 
     createSensor: function(req, res){
-        new sensorlModel({
-            _id: new ObjectID(),
-            name: req.body.name ? req.body.name : req.body.ip,
-            ip: req.body.ip,
-            protocol: req.body.protocol ? req.body.protocol : '',
-            user: req.body.username
-        }).save(function(error, sensors){
-            if(error || !sensors){
-                res.status(401).json('Error network');
-                res.end();
-            }else{
-                userModel.findOne({username: req.body.username}).exec((error, users)=>{
-                    if(error || !users) return res.status(500).json(error)
-                    else {
-                        users.sensor.addToSet(sensors._id)
-                        users.save()
-                        return res.status(200).json(users)
-                    }
-                })
-            }
-        })
+        if(req.body.ip && checkip(req.body.ip)) {
+            new sensorlModel({
+                _id: new ObjectID(),
+                name: req.body.name ? req.body.name : req.body.ip,
+                ip: req.body.ip,
+                protocol: req.body.protocol ? req.body.protocol : '',
+                user: req.body.username
+            }).save(function(error, sensors){
+                if(error || !sensors){
+                    res.status(401).json({status: error.toString()});
+                    res.end();
+                }else{
+                    userModel.findOne({username: req.body.username}).exec((error, users)=>{
+                        if(error || !users) return res.status(500).json(error)
+                        else {
+                            users.sensor.addToSet(sensors._id)
+                            users.save()
+                            return res.status(200).json(users)
+                        }
+                    })
+                }
+            })
+        } else res.status(412).json({status:'ip is require'})
+        
     },
 
     getSensor: function(req, res){
