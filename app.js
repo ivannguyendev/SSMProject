0996b4068139540d625e3d60c5a147836bd81c9c
tungnv211996 <tungnv21199@gmail.com>
2017-12-12 15:57:00 +0700

    fix login css

--- app.js
+++ app.js
@@ -75,5 +75,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3006, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
 module.exports = app;
